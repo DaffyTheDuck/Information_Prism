960f1cf1d35b16bb8f3afc1f14eecea227640604
DaffyTheDuck <34976093+DaffyTheDuck@users.noreply.github.com>
2025-01-21 18:31:30 +0530

    Added Loader Functions
    - Added WikiLoader
    - Improved WebLoader

--- DataLoader.py
+++ DataLoader.py
@@ -4,19 +4,29 @@ import requests
 import bs4
 import requests
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
-import time
 
-from langchain_community.document_loaders import WebBaseLoader
+import nltk
+import spacy
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+"""
+Refer following for various document loaders
+
+https://python.langchain.com/docs/integrations/document_loaders/
+"""
+
+from langchain_community.document_loaders import WikipediaLoader
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
 
 
 dotenv.load_dotenv()
 
-class WebLoader:
+class DataLoader:
 
     def __init__(self, user_input):
         self.CSE_API_KEY = os.getenv("CSE_API_KEY")
@@ -27,6 +37,13 @@ class WebLoader:
         self.options.add_argument("--headless")
         self.service = Service(GeckoDriverManager().install())
         self.driver = webdriver.Firefox(service=self.service, options=self.options)
+
+        # NLP for User Query
+        nltk.download('stopwords')
+        self.stop_words = set(stopwords.words('english'))
+        # subprocess.run("python3 -m spacy download en_core_web_sm")
+        os.system("python -m spacy download en_core_web_sm")
+        self.model = spacy.load("en_core_web_sm")
 
         self.user_input = user_input
 
@@ -86,6 +103,24 @@ class WebLoader:
             print(f"Error occurred: {e}")
             return []
     
+
+    def process_user_input(self):
+        # Tokenize the data
+        self.tokenized_input = word_tokenize(self.user_input)
+        # Stopwords removal
+        self.processed_user_input = ' '.join([word.lower() for word in self.tokenized_input if word not in self.stop_words])
+
+        self.doc = self.model(self.processed_user_input)
+        
+        self.entities = [ent.text for ent in self.doc.ents]
+
+        self.noun_phrases = [chunk.text for chunk in self.doc.noun_chunks]
+
+        # Combine and remove duplicates
+        self.phrases = list(dict.fromkeys([self.processed_user_input] + self.entities + self.noun_phrases))
+        return self.phrases
+
+
     def links_list(self):
         self.get_links = []
         self.contetn = []
@@ -95,32 +130,9 @@ class WebLoader:
             self.get_links.append(self.data[i]["link"])
 
         return self.get_links
-        
-    def process_data(self):
-        
-        self.get_links = self.links_list()
-        
-        for i in range(len(self.get_links)):
-            self.loader = WebBaseLoader(self.get_links[i])
-            self.loader.requests_kwargs = {
-                'verify': True,
-                'headers': {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-                    'Accept-Encoding': 'gzip, deflate',
-                }
-            }
-            self.docs = self.loader.load()
-            try:
-                self.processed_title = f"{self.docs[0].metadata['title']}.txt".strip()
-                self.processed_content = self.docs[0].page_content.strip()
-                os.mkdir("WebLoader")
-                with open(f"WebLoader/{self.processed_title}.txt", "w", encoding="utf-8") as f:
-                    f.write(self.processed_content)
-            except FileExistsError:
-                with open(f"WebLoader/{self.processed_title}.txt", "w", encoding="utf-8") as f:
-                    f.write(self.processed_content)
     
-    def use_selenium(self):
+
+    def load_from_web(self):
         self.get_links = self.links_list()
 
         for i in range(len(self.get_links)):
@@ -146,7 +158,36 @@ class WebLoader:
                 with open(f"WebLoader/{self.title}.txt", "w", encoding="utf-8") as f:
                     f.write(self.content)
         self.driver.close()
+    
 
-wl = WebLoader("new virus spreading china")
-wl.use_selenium()
+    def load_from_wikipedia(self):
+        self.keywords = self.process_user_input()
+        self.all_docs = []
 
+        for keyword in self.keywords:
+            try:
+                self.loader = WikipediaLoader(query=keyword, load_max_docs=1, doc_content_chars_max=40000000)
+                self.docs = self.loader.load()
+
+                if not self.docs:
+                    print(f"No documents found for: {keyword}")
+                    continue
+                try:
+                    os.mkdir("WikiLoader")
+                    with open(f"WikiLoader/{self.docs[0].metadata['title']}.txt", "w", encoding="utf-8") as f:
+                        f.write(self.docs[0].page_content)
+                except FileExistsError:
+                    with open(f"WikiLoader/{self.docs[0].metadata['title']}.txt", "w", encoding="utf-8") as f:
+                        f.write(self.docs[0].page_content)
+                self.all_docs.append(self.docs)
+
+            except Exception as e:
+                print(f"Problem fetching the document: {e}")
+                continue
+        
+        if len(self.all_docs) == 0:
+            print("No Documents Were Loaded")
+
+
+dl = DataLoader("47th President of The United States")
+dl.load_from_wikipedia()
